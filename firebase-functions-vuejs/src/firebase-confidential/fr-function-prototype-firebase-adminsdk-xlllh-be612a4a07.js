686fb488a2714b307c421912cc63ea57f6e27f56
Dee Ker <dker95@icloud.com>
2020-06-22 01:17:18 +0800

    add firebase-confidential folder back, prepared for heroku hosting

---
+++ firebase-functions-vuejs/src/firebase-confidential/fr-function-prototype-firebase-adminsdk-xlllh-be612a4a07.js
@@ -0,0 +1,23 @@
+// export default
+//   {
+//     "type": "service_account",
+//     "project_id": "fr-function-prototype",
+//     "private_key_id": "be612a4a072ed4a9dfee5d2f782d6c1980d0d14c",
+//     "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCtID1Ce0XOXjCS\n0xfTcZDIA581O2NMzkLuS5O7RDu0WTHcXHIOYC3Ckub2DXj9jrE1haW6CR31kv46\n5PQn9/jBOSO+KjLc9I9eEK7vRZFLZEywDNbnTzswCbaUPyYGYzuBbPnuWN2/IjLa\nOLC6IXvd5PoMmyhJVW02Dln9b9M9kT+Seytmn4UrR9SbbwgUMGfaZnZSZQC+0VPj\n/kox6MUMCOYR7Dd6SO/JaCQZPd2xCPxymbsAu27TBb6Go24GlqtUBkCyTxnkGSOg\n0xtcUGzGrZ0FtVQ0Gktel/OJI+QsUdN4kkg0EUiJz0PVsikOuTTX7BQdUf5Zkfj3\nfcFHjjtpAgMBAAECggEAGXN0XgomTHzPUj5jMcvvt1/E73vwELH/5+1HZg/VYSF9\nsYILqG5eO80B1ClN4KSyC4a+wtclRt1Zr4nKFPgg4zMFvKaSfKYdT3xPY1CMWBOC\nLJ1umGmu71R2OVwBOI4tCM1e/rmku70I0cjkvjA+9/N6CRFkPkenOvuoIhhm6xVp\n3G48f0XdeZfHh58vi0XhUHD3Cr0DXNHpBPSTq9b9eGSbZ6fEAVsVHoy6i2RmsugR\nrbRpVk6ixgPY9rqX0LKmnqJdHgGbI8hDs1p2uD7vyq+HFqf0hrSnMtoVgkrQuZiH\nhzanhR99+0f3TrV9QhQaNxAQu2SLQ2P9Wz+9lpaUKwKBgQDakucvWXtEecDVyo+V\nG+ZFYZHJkRLA6+PW0K83mTT7p/KN01Ty9VBQv+GChDFlZ/FSpp4CyNQ9g5uvzGup\n5z4iwaYsLFd6WiFS2eh28+v2G+JQcBSAFGQL9AO7E+2+ef+sgixRwvJZAOdcZrgG\nWmkITMIOAT5tTzHFnsOUH23TowKBgQDKxSU84n+f2JEhB2eK0ZBpwQECYBiMHT1y\nDU0dxZpxXR+i/tr64zCzS1H28FysK7zJny6P+9pbSeXGA2t/v0CIAZbjy5PsnJIF\nlYeYoZUuSLYgwB1IfPIFqerLCoGfSeOxSRwJEv38EbIYVUZegEArm/aFZmbnJWsk\nLm/IC3BFgwKBgQChEdaJvxAmou5m3dG3WQr1H/SWLHE8d9i7iadFeoJf3SDSQOT9\n8acRCoW9b3sTNmjQtnYEqUg2WJDXGPZSC1VvuXKiZL7GLDsYWfqEBjZhc6TwD9gb\nlmsvn89MAmCVmEPANWFBkz6SNidIoW2FSQTTl/T86w+877pG3jtUIWEGrwKBgHrX\n5S91qQwWEBaKfdhEDdGD9DCUN0rrjAZEuKuIw4BagoglDlVgvmhhAU8xX3OPK9ia\nb4MbebZElsfX2pCSNW4wSFHJS9zVHkHzzSpS+iYINw6wVQ1NZPK5dl18B/uQsDj/\nqdR1hRnpPpQvVWp7GGVTH47B8yWmLbrFx0LSazoxAoGADD63CuVxsgkYKC7cIRlx\nle0S2T9FOvDz5Hvw4emuI3uAxiPQ/uQ1HQfFARAERAPLRstriOqqSLlQ6tOfOgrA\n+jVIg0dMVj4ph9jCbcMWdWQ6bsr9YchINfjQI265H/dJeoMxj8MbCbgVnIAaEmTH\njKXYNJmOe27B8erUY0j05Gs=\n-----END PRIVATE KEY-----\n",
+//     "client_email": "firebase-adminsdk-xlllh@fr-function-prototype.iam.gserviceaccount.com",
+//     "client_id": "118341176457435554713",
+//     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+//     "token_uri": "https://oauth2.googleapis.com/token",
+//     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+//     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-xlllh%40fr-function-prototype.iam.gserviceaccount.com"
+//   }
+
+export default {
+  apiKey: "AIzaSyAi8on9N2YTH132xkSe7YO-9GvWbyXAPjM",
+  authDomain: "fr-function-prototype.firebaseapp.com",
+  databaseURL: "https://fr-function-prototype.firebaseio.com",
+  projectId: "fr-function-prototype",
+  storageBucket: "fr-function-prototype.appspot.com",
+  messagingSenderId: "552615027293",
+  appId: "1:552615027293:web:048a96ba1406c53ee1a3b4"
+}
